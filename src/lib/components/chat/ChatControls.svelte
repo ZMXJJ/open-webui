33d45bf88440538bb99468186519f91285ec8a49
EnjoyCloudDev <enjoycloud@foxmail.com>
2025-05-07 21:54:56 +0800

    feat: 更新字体和UI样式以提升用户体验

--- src/lib/components/chat/ChatControls.svelte
+++ src/lib/components/chat/ChatControls.svelte
@@ -196,7 +196,7 @@
 		{#if $showControls}
 			<PaneResizer class="relative flex w-2 items-center justify-center bg-background group">
 				<div class="z-10 flex h-7 w-5 items-center justify-center rounded-xs">
-					<EllipsisVertical className="size-4 invisible group-hover:visible" />
+					<EllipsisVertical className="size-4 invisible group-hover:visible text-[#647fff]" />
 				</div>
 			</PaneResizer>
 		{/if}
--- src/lib/components/chat/MessageInput.svelte
+++ src/lib/components/chat/MessageInput.svelte
@@ -1265,8 +1265,9 @@
 										{:else if prompt === '' && files.length === 0 && ($_user?.role === 'admin' || ($_user?.permissions?.chat?.call ?? true))}
 											<div class=" flex items-center">
 												<Tooltip content={$i18n.t('Call')}>
+													<!-- Call button -->
 													<button
-    													class=" bg-[#647fff] text-white border border-[#647fff] rounded-full group-hover:visible invisible transition"
+    													class=" bg-[#647fff] text-white hover:bg-[#4868ff] dark:bg-[#647fff] dark:text-white dark:hover:bg-[#4868ff] transition rounded-full p-1.5 self-center"
     													type="button"
 														on:click={async () => {
 															if (selectedModels.length > 1) {
@@ -1320,12 +1321,16 @@
 														}}
 														aria-label="Call"
 													>
-														<Headphone className="size-5" />
+														<!-- <Headphone className="size-5" /> -->
+														<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-5">
+															<path fill-rule="evenodd" d="M1.5 4.5a3 3 0 0 1 3-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 0 1-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 0 0 6.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 0 1 1.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 0 1-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5Z" clip-rule="evenodd" />
+														</svg>
+														  
 													</button>
 												</Tooltip>
 											</div>
 										{:else}
-											<!-- 发送按钮 -->
+											<!-- MsgSend Button -->
 											<div class=" flex items-center">
 												<Tooltip content={$i18n.t('Send message')}>
 													<button
